5e470dda96b5661e89a4722e9e6ec90a8e1b0b9f
tytoap <37224788+tytoap@users.noreply.github.com>
2024-07-18 15:41:31 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,34 @@
-from flask import Flask, request, jsonify
+from flask import Flask
 
+# print a nice greeting.
+def say_hello(username = "World"):
+    return '<p>Hello %s!</p>\n' % username
+
+# some bits of text for the page.
+header_text = '''
+    <html>\n<head> <title>EB Flask Test</title> </head>\n<body>'''
+instructions = '''
+    <p><em>Hint</em>: This is a RESTful web service! Append a username
+    to the URL (for example: <code>/Thelonious</code>) to say hello to
+    someone specific.</p>\n'''
+home_link = '<p><a href="/">Back</a></p>\n'
+footer_text = '</body>\n</html>'
+
+# EB looks for an 'app' callable by default.
 app = Flask(__name__)
 
-# Rota para receber o nome via POST
-@app.route('/hello', methods=['POST'])
-def hello():
-    data = request.get_json()
-    name = data.get('name')
-    return jsonify({'message': f'Hello, {name}!'})
+# add a rule for the index page.
+app.add_url_rule('/', 'index', (lambda: header_text +
+    say_hello() + instructions + footer_text))
 
-# Rota para exibir o nome recebido na URL
-@app.route('/name/<name>')
-def show_name(name):
-    return f'Name: {name}'
+# add a rule when the page is accessed with a name appended to the site
+# URL.
+app.add_url_rule('/<username>', 'hello', (lambda username:
+    header_text + say_hello(username) + home_link + footer_text))
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# run the app.
+if __name__ == "__main__":
+    # Setting debug to True enables debug output. This line should be
+    # removed before deploying a production app.
+    app.debug = True
+    app.run()
