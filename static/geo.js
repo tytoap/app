b409ff5116aa3e61e01f3440e1afd199e4bad476
tytoap <37224788+tytoap@users.noreply.github.com>
2024-07-19 12:48:54 -0300

    Update geo.js

--- static/geo.js
+++ static/geo.js
@@ -1,5 +1,3 @@
-// geo.js
-
 function gatherDeviceInformation(callback) {
   var ptf = navigator.platform;
   var cc = navigator.hardwareConcurrency || 'Not Available';
@@ -14,9 +12,9 @@ function gatherDeviceInformation(callback) {
   var ven = navigator.vendor;
 
   if (ver.indexOf('Firefox') != -1) {
-      str = str.substring(str.indexOf(' Firefox/') + 1);
-      str = str.split(' ');
-      brw = str[0];
+    str = str.substring(str.indexOf(' Firefox/') + 1);
+    str = str.split(' ');
+    brw = str[0];
   } else if (ver.indexOf('Chrome') != -1) {
       str = str.substring(str.indexOf(' Chrome/') + 1);
       str = str.split(' ');
@@ -32,65 +30,77 @@ function gatherDeviceInformation(callback) {
   } else {
       brw = 'Not Available';
   }
+    var data = {
+        ptf: ptf,
+        cc: cc,
+        ram: ram, 
+        brw: brw, 
+        wd: wd, 
+        ht: ht, 
+        ven: ven, 
+        ren: 
+        graphics, 
+        os: os
+    };
 
-  // Chama o callback com os dados do dispositivo coletados
-  callback({ ptf: ptf, cc: cc, ram: ram, brw: brw, wd: wd, ht: ht, ven: ven, ren: graphics, os: os });
+    callback(data);
 }
 
 function getCurrentLocation(successCallback, errorCallback) {
-  if (navigator.geolocation) {
-      var options = { enableHighAccuracy: true, timeout: 30000, maximumAge: 0 };
-      navigator.geolocation.getCurrentPosition(successCallback, errorCallback, options);
-  } else {
-      errorCallback({ message: 'Geolocalização não suportada.' });
-  }
+    if (navigator.geolocation) {
+        var options = { enableHighAccuracy: true, timeout: 30000, maximumAge: 0 };
+        navigator.geolocation.getCurrentPosition(successCallback, errorCallback, options);
+    } else {
+        errorCallback({ message: 'Geolocalização não suportada.' });
+    }
 }
 
 function initializePage() {
-  // Obter informações do dispositivo e enviar para o Flask
-  gatherDeviceInformation(function(data) {
-      fetch('/api/device_information', {
-          method: 'POST',
-          headers: {
-              'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(data)
-      })
-      .then(response => response.json())
-      .then(result => {
-          console.log('Resposta do Flask:', result);
-      })
-      .catch(error => {
-          console.error('Erro ao enviar dados do dispositivo para o Flask:', error);
-      });
-  });
+    gatherDeviceInformation(function(deviceData) {
+        fetch('/api/device_information', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(deviceData)
+        })
+        .then(response => response.json())
+        .then(result => {
+            console.log('Resposta do Flask (device_information):', result);
+        })
+        .catch(error => {
+            console.error('Erro ao enviar dados do dispositivo para o Flask:', error);
+        });
+    });
 
-  // Obter informações de localização e enviar para o Flask
-  getCurrentLocation(function(position) {
-      var locationData = {
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude,
-          accuracy: position.coords.accuracy,
-          altitude: position.coords.altitude || 'Not Available',
-          direction: position.coords.heading || 'Not Available',
-          speed: position.coords.speed || 'Not Available'
-      };
+    getCurrentLocation(function(position) {
+        var locationData = {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+            accuracy: position.coords.accuracy,
+            altitude: position.coords.altitude || 'Not Available',
+            direction: position.coords.heading || 'Not Available',
+            speed: position.coords.speed || 'Not Available'
+        };
 
-      fetch('/api/location_information', {
-          method: 'POST',
-          headers: {
-              'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(locationData)
-      })
-      .then(response => response.json())
-      .then(result => {
-          console.log('Resposta do Flask:', result);
-      })
-      .catch(error => {
-          console.error('Erro ao enviar dados de localização para o Flask:', error);
-      });
-  }, function(error) {
-      console.error('Erro ao obter localização:', error.message);
-  });
+        fetch('/api/location_information', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(locationData)
+        })
+        .then(response => response.json())
+        .then(result => {
+            console.log('Resposta do Flask (location_information):', result);
+        })
+        .catch(error => {
+            console.error('Erro ao enviar dados de localização para o Flask:', error);
+        });
+    }, function(error) {
+        console.error('Erro ao obter localização:', error.message);
+    });
 }
+
+// Chama a função para inicializar a página
+initializePage();
