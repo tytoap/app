d17aded1cc724e42576067eca2eaa8dc447d6a0a
tytoap <37224788+tytoap@users.noreply.github.com>
2024-08-08 13:30:06 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,25 +1,41 @@
 from flask import Flask, jsonify, render_template, request
 import requests
+from requests.auth import HTTPProxyAuth
 
 app = Flask(__name__)
+
+# Configurações do proxy com autenticação
+PROXY_URL = 'http://proxy.educacao.parana:8080'  # Substitua pelo URL do seu proxy
+PROXY_USER = 'joailson'           # Substitua pelo seu nome de usuário
+PROXY_PASS = 'Axl130213a'           # Substitua pela sua senha
+
+ 
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/api/ip-location')
+@app.route('/api/ip-location', methods=['POST'])
 def ip_location():
-    # Captura o IP do cliente
-    client_ip = request.remote_addr
+    data = request.json
+    client_ip = data.get('ip')
+
+    if not client_ip:
+        return jsonify({'error': 'IP address not provided'}), 400
+
     try:
-        # Requisição para a API ip-api usando o IP do cliente
-        response = requests.get(f'http://ip-api.com/json/{client_ip}')
+        # Requisição para a API ipinfo.io para obter a localização baseada no IP
+        proxies = {
+            'http': PROXY_URL,
+            'https': PROXY_URL
+        }
+        response = requests.get(f'https://ipinfo.io/{client_ip}/json', proxies=proxies, auth=(PROXY_USER, PROXY_PASS))
         response.raise_for_status()  # Verifica se a resposta foi bem-sucedida
-        data = response.json()
-        return jsonify(data)
+        location_data = response.json()
+        return jsonify(location_data)
     except requests.RequestException as e:
         print(f"Request failed: {e}")
-        return jsonify({'error': 'Failed to get IP location data'}), 500
+        return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
