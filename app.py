127e059196ba7ccda9b5d6110dcf6f5f503dab8d
tytoap <37224788+tytoap@users.noreply.github.com>
2024-08-08 15:33:49 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,33 @@
-from flask import Flask, jsonify, render_template, request
-import requests
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # Configura o CORS para permitir todas as origens. Pode ser configurado mais especificamente se necessário.
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+# Variável global para armazenar os dados
+stored_data = {}
 
-@app.route('/api/ip-location')
-def ip_location():
-    # Captura o IP do cliente
-    client_ip = request.remote_addr
+@app.route('/submit_data', methods=['POST'])
+def submit_data():
+    global stored_data
     try:
-        # Requisição para a API ip-api usando o IP do cliente
-        response = requests.get(f'http://ip-api.com/json/{client_ip}')
-        response.raise_for_status()  # Verifica se a resposta foi bem-sucedida
-        data = response.json()
-        return jsonify(data)
-    except requests.RequestException as e:
-        print(f"Request failed: {e}")
-        return jsonify({'error': 'Failed to get IP location data'}), 500
+        data = request.json
+        stored_data = data  # Armazena os dados recebidos na variável global
+        print(f"Data received and stored: {data}")  # Adiciona uma mensagem de log
+        return jsonify({"status": "success", "data": data}), 200
+    except Exception as e:
+        print(f"Error receiving data: {str(e)}")  # Adiciona uma mensagem de log de erro
+        return jsonify({"status": "error", "message": str(e)}), 500
+
+@app.route('/display_results', methods=['GET'])
+def display_results():
+    global stored_data
+    if stored_data:
+        print(f"Data being returned: {stored_data}")  # Adiciona uma mensagem de log
+        return jsonify(stored_data), 200
+    else:
+        print("No data available")  # Adiciona uma mensagem de log
+        return jsonify({"status": "error", "message": "No data available"}), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
