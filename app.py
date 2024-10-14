5f28f973e7f5eabc8bae302f488e3434c8dbebe9
tytoap <37224788+tytoap@users.noreply.github.com>
2024-10-14 10:29:33 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,37 +1,38 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify
 from flask_cors import CORS
+
 
 app = Flask(__name__)
 CORS(app)  # Configura o CORS para permitir todas as origens. Pode ser configurado mais especificamente se necessário.
 
-# Variável global para armazenar os dados
-stored_data = {}
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/submit_data', methods=['POST'])
-def submit_data():
-    global stored_data
-    try:
-        data = request.json
-        stored_data = data  # Armazena os dados recebidos na variável global
-        print(f"Data received and stored: {data}")  # Adiciona uma mensagem de log
-        return jsonify({"status": "success", "data": data}), 200
-    except Exception as e:
-        print(f"Error receiving data: {str(e)}")  # Adiciona uma mensagem de log de erro
-        return jsonify({"status": "error", "message": str(e)}), 500
-
-@app.route('/display_results', methods=['GET'])
-def display_results():
-    global stored_data
-    if stored_data:
-        print(f"Data being returned: {stored_data}")  # Adiciona uma mensagem de log
-        return jsonify(stored_data), 200
+# Rota para receber as coordenadas do mapa
+@app.route('/location', methods=['POST'])
+def receive_location():
+    data = request.json  # Recebendo os dados em JSON
+    google_map_link = data.get('data')
+    
+    if google_map_link:
+        # Aqui você pode fazer o que quiser com o link do Google Maps
+        print(f"Google Map Link Received: {google_map_link}")
+        return jsonify({"status": "success", "message": "Location received"}), 200
     else:
-        print("No data available")  # Adiciona uma mensagem de log
-        return jsonify({"status": "error", "message": "No data available"}), 404
+        return jsonify({"status": "error", "message": "No data received"}), 400
+
+# Rota para lidar com os erros
+@app.route('/receive-error', methods=['POST'])
+def receive_error():
+    data = request.json  # Recebendo os dados em JSON
+    denied = data.get('Denied')
+    unavailable = data.get('Una')
+    timeout = data.get('Time')
+    unknown = data.get('Unk')
+
+    # Processando os erros
+    error_message = f"Permission Denied: {denied}, Position Unavailable: {unavailable}, Timeout: {timeout}, Unknown: {unknown}"
+    print(f"Error Occurred: {error_message}")
+    
+    return jsonify({"status": "success", "message": "Error received", "error": error_message}), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
